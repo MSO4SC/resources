682781597c6ab3ae396a7d7efff2f05288fbdb66
Javier Carnero <javier.carnero@atos.net>
2017-11-07 10:48:49 +0100

    Doc written.

--- blueprint-examples/force-cancel.sh
+++ blueprint-examples/force-cancel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://10.38.3.149/executions/$1 -X PATCH \
+curl http://10.38.3.74/executions/$1 -X PATCH \
 	--header "Tenant: default_tenant" \
 	-u admin:admin \
 	-H "Content-Type: application/json" -d '{"status": "cancelled"}'
