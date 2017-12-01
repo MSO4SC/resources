cec4cd1527fd52329f5ec06542e58579b9663eff
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-12-01 13:07:18 +0100

    Update feelpp scripts

--- blueprint/feelpp/torsionbar/create_sbatch.sh
+++ blueprint/feelpp/torsionbar/create_sbatch.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -l
 
+# Singularity image retrieved from
+# https://www.singularity-hub.org/collections/253
+
+module load singularity/2.3.1
+
+SLURMDIR=$1
+IMAGENAME=$2
+IMAGEDEST=$3
+
+IMAGENAME23=$(basename ${IMAGENAME}).2.3.1
+
+cd ${IMAGEDEST}
+if [ -f "${IMAGENAME}" ]; then
+  singularity pull -name "${IMAGENAME}" shub://feelpp/singularity:feelpp-toolboxes-latest
+fi
+if [ -f "${IMAGENAME23}" ]; then
+  singularity build --writable ${IMAGENAME23} ${IMAGENAME}
+fi
+
 sed -i s/FEELPP_SINGULARITY_IMAGE=.*/FEELPP_SINGULARITY_IMAGE=$3/ $2
