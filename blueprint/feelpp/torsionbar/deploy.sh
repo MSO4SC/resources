ff5884023e6a90470f7ce90a6e2ec47d4cabce2d
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-12-05 14:18:34 +0100

    Update deploy script
    - Add ignore file to avoid pushing the local yaml with login/password on
    the repo

--- blueprint/feelpp/torsionbar/deploy.sh
+++ blueprint/feelpp/torsionbar/deploy.sh
@@ -4,19 +4,25 @@
 
 arg=$1
 
-ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+ROOTDIR=${SCRIPTDIR}/../../../
 JOB=torsionbar_job
 TOSCA=blueprint.yaml
-LOCAL=mylocal.yaml
+LOCAL=local-blueprint-inputs.yaml
+
+if [ ! -f "${ROOTDIR}/${LOCAL}" ]; then
+    echo "${ROOTDIR}/${LOCAL} does not exist! See doc or blueprint examples!"
+    exit 1
+fi
 
 mkdir -p ${JOB}
 cd ${JOB}
 case $arg in
     "up" )
-        cfy blueprints upload -b "${JOB}" "${ROOTDIR}/${TOSCA}"
+        cfy blueprints upload -b "${JOB}" "${SCRIPTDIR}/${TOSCA}"
         read -n 1 -s -p "Press any key to continue"
         echo ''
-        cfy deployments create -b "${JOB}" -i "${ROOTDIR}/${LOCAL}" --skip-plugins-validation "${JOB}"
+        cfy deployments create -b "${JOB}" -i "../../../../${LOCAL}" --skip-plugins-validation ${JOB}
         read -n 1 -s -p "Press any key to continue"
         echo ''
         cfy executions start -d "${JOB}" install
