c5fbc72eb56382d52ee4466da9a19fae88697744
Javier Carnero <javier.carnero@atos.net>
2018-01-08 11:40:51 +0100

    Comment updated in force-cancel.sh

--- blueprint-examples/force-cancel.sh
+++ blueprint-examples/force-cancel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## execute as ./force-cancel.sh mso4scpower [EXEC-ID]
+## execute as ./force-cancel.sh [ADMIN PASSWORD] [EXEC-ID]
 
 curl http://193.144.35.131/executions/$2 -X PATCH \
 	--header "Tenant: default_tenant" \
