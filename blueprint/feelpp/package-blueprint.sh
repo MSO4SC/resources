6d908d0be90611a2f26519fa75eab6c3641165eb
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2018-01-21 17:39:15 +0100

    Prepare for modular Hifimagnet apps

--- blueprint/feelpp/package-blueprint.sh
+++ blueprint/feelpp/package-blueprint.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 usage(){
    echo ""
    echo "Description:"
-   echo "               Package an application for MSO portal deployement"
+   echo " Package an application for MSO portal deployement"
    echo ""
    echo "Usage:"
    echo ""
-   echo ""
-   echo "-h             Prints this help information"
+   echo "-f define directory where the package is stored"
+   echo "-n name of the package (default is upload)"
+   echo "-o specify the output directory name (if different from input)"
+   echo "-h Prints this help information"
    echo ""
    exit 1
 }
@@ -18,11 +20,12 @@ DEBUG=0
 #########################################################
 ## parse parameters
 ##########################################################
-while getopts "hd:o:" option ; do
+while getopts "hdf:o:n:" option ; do
    case $option in
        h ) usage ;;
        f ) DIRECTORY=$OPTARG ;;
-       o ) OUTPUT==$OPTARG ;;
+       n ) PACKAGE=$OPTARG ;;
+       o ) OUTPUT=$OPTARG ;;
        d ) DEBUG=1 ;;
        ? ) usage ;;
    esac
@@ -31,10 +34,32 @@ done
 shift $((OPTIND - 1))
 
 : ${DIRECTORY:="hifimagnet_test"}
+: ${PACKAGE:="upload"}
 : ${OUTPUT:=$DIRECTORY}
 
 export COPYFILE_DISABLE=true
 
-cd $DIRECTORY || exit 1
-tar --exclude=deploy.sh --exclude=README.adoc -czf ../$OUTPUT.tar.gz upload || exit 1
-cd ..
+startupdir=$(echo $PWD)
+
+if [ ! -d $DIRECTORY ]; then
+    echo "no such directory: $DIRECTORY"
+    exit 1
+fi
+cd $DIRECTORY
+
+if [ ! -d $PACKAGE ]; then
+    echo "no such package: $PACKAGE"
+    exit 1
+fi
+
+echo -n "Package $PACKAGE into ../$OUTPUT.tar.gz"
+tar --exclude=deploy.sh --exclude=README.adoc -czf ../$OUTPUT.tar.gz $PACKAGE
+isOK=$?
+cd $startupdir
+
+if [ $isOK = "0" ]; then
+    echo "\t OK"
+else
+    echo "\t FAILED"
+    exit
+fi
