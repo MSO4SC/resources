4744a2361495eeb85a655e37545c2ec43fde9b61
ncde <ncde@kth.se>
2018-06-14 09:29:06 +0200

    workdir fixed, problem with execution

--- blueprint/floatingWindTurbine/floatingWT/scripts/bootstrap_1.sh
+++ blueprint/floatingWindTurbine/floatingWT/scripts/bootstrap_1.sh
@@ -1,15 +1,30 @@
 #!/bin/bash -l
 
-module load singularity/2.4.2
+echo "started bootstrap " > $LUSTRE/bootstraplog
 
+echo $1 >> $LUSTRE/bootstraplog
+echo $2 >> $LUSTRE/bootstraplog
+echo $3 >> $LUSTRE/bootstraplog
+echo $4 >> $LUSTRE/bootstraplog
+echo $5 >> $LUSTRE/bootstraplog
+echo $6 >> $LUSTRE/bootstraplog
+echo $7 >> $LUSTRE/bootstraplog
+echo $8 >> $LUSTRE/bootstraplog
+echo $9 >> $LUSTRE/bootstraplog
+
+module load singularity/2.4.2
 SINGULARITY_PULLFOLDER=$1
 singularity pull shub://sregistry.srv.cesga.es/mso4sc/fenics-unicorn:latest
 
+echo "pull complete " >> $LUSTRE/bootstraplog
+
+module load git
 git clone https://bitbucket.org/fenics-hpc/unicorn.git
 cd unicorn
 git fetch --all
 git checkout unicorn_var_den_ale
 
+echo "git complete " >> $LUSTRE/bootstraplog
 echo $2 > meshaddress
 
 echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > parameters.xml
