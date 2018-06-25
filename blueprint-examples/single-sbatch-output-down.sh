762ec3f37e641dd34e96e32fafa4dff2140d4e72
Javier Carnero <javier.carnero@atos.net>
2018-06-25 14:49:48 +0200

    New publish output example
    Fixes and improvements in the rest of examples

---
+++ blueprint-examples/single-sbatch-output-down.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd single-sbatch-output
+echo "Uninstalling deployment.."
+cfy executions start -d single-sbatch-output uninstall
+echo "Deleting deployment.."
+cfy deployments delete single-sbatch-output
+echo "Deleting blueprint.."
+cfy blueprints delete single-sbatch-output
--- blueprint-examples/single-singularity-sregistry-job/scripts/singularity_revert_example.sh
+++ blueprint-examples/single-singularity-sregistry-job/scripts/singularity_revert_example.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
 
-URI=$1
-FILENAME=$2
-
-rm -f $FILENAME &> revert.log
+if [ -f $1/$2 ]; then
+    rm $1/$2
+fi
